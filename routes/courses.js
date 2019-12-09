550c63660668b24c654cdf70384c565e79d2bca2
DebarunMitra <debarun96mitra@gmail.com>
2019-12-09 22:45:53 +0530

    sourses filter added

--- routes/courses.js
+++ routes/courses.js
@@ -4,9 +4,14 @@ const mongoose = require('mongoose');
 const Courses = mongoose.model('courses');
 
 router.get('/',(req, res) => {
-  Courses.find({provider:{$ne:''},
+  Courses.find({
+  provider:{$ne:''},
   universities_institutions:{$ne:''},
-  
+  child_subject:{$ne:''},
+  url:{$ne:''},
+  next_session_date:{$ne:''},
+  length:{$ne:''},
+  video_url:{$ne:''}
 }).then(async courses => {
   let len=await courses.length;
     console.log(len);
