620b4a6ba190cc1e0f850482beae05e25f7a3d26
DebarunMitra <debarun96mitra@gmail.com>
2019-12-10 18:30:54 +0530

    auth modification

--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,6 @@ router.get('/google', passport.authenticate('google', {scope: ['profile', 'email
 
 router.get('/google/callback',
   passport.authenticate('google', { failureRedirect: '/' }),(req, res) => {
-  //  res.redirect('/profile');
       res.redirect('/');
   });
 
