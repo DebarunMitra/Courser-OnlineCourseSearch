8892db60bdf9c40e8e2c74098c62501148d4ef21
DebarunMitra <debarun96mitra@gmail.com>
2019-12-10 20:33:01 +0530

    courses modified

--- routes/courses.js
+++ routes/courses.js
@@ -29,29 +29,55 @@ router.get('/',(req, res) => {
 
 /**
  * [/courses]
- * @return [return all courses from database]
+ * @return [return searched subject courses from database]
  */
-router.get('/:subject',(req, res) => {
-  Courses.find({
+router.get('/search/:subject',(req, res) => {
+  const {page, perPage}=req.query;
+  const options={
+    page: (parseInt(page,10))?parseInt(page,10):1,
+    limit:(parseInt(perPage,10))?parseInt(perPage,10):10
+  }
+  let subject=(req.params.subject!=='blank')?req.params.subject:".*?";
+  console.log(subject);
+  Courses.paginate({
     provider:{$ne:''},
     universities_institutions:{$ne:''},
-    child_subject:{ "$regex":req.params.subject, "$options": "i" },
+    child_subject:{ "$regex":subject, "$options": "i" },
     url:{$ne:''},
     length:{$ne:null},
     next_session_date:{$ne:''},
     video_url:{$ne:''}
-}).then(async courses => {
-  let len=await courses.length;
+},options).then(async courses => {
+  let len=await courses.docs.length;
     console.log(len);
     await res.status(200).send(courses);
   });
 });
 
-
-
-
-
-
-
+/**
+ * [/courses]
+ * @return [return searched provider courses from database]
+ */
+router.get('/search/provider/:provider',(req, res) => {
+  const {page, perPage}=req.query;
+  const options={
+    page: (parseInt(page,10))?parseInt(page,10):1,
+    limit:(parseInt(perPage,10))?parseInt(perPage,10):10
+  }
+  let provider=(req.params.provider!=='blank')?req.params.provider:".*?";
+  Courses.paginate({
+    provider:{ "$regex":provider, "$options": "i" },
+    universities_institutions:{$ne:''},
+    child_subject:{$ne:''},
+    url:{$ne:''},
+    length:{$ne:null},
+    next_session_date:{$ne:''},
+    video_url:{$ne:''}
+},options).then(async provider => {
+  let len=await provider.docs.length;
+    console.log(len);
+    await res.status(200).send(provider);
+  });
+});
 
 module.exports = router;
