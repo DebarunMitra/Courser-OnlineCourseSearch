de74eba38d7b5c56d0f2d28be30a64f50b3d8074
DebarunMitra <debarun96mitra@gmail.com>
2019-12-11 11:07:46 +0530

    courses added

--- client/src/actions/myactions.js
+++ client/src/actions/myactions.js
@@ -2,11 +2,21 @@ import axios from 'axios';
 
 export const fetchUserAction = ()=>{
    return (dispatch)=>{
-     
+
     axios.get('/auth/verify')
     .then((res)=>{
        dispatch({type:'GET_USER',payload:res.data})
     })
+  }
+}
 
-   }
+
+export const fetchCourses = ()=>{
+  return (dispatch)=>{
+    axios.get('/courses')
+      .then((res)=>{
+        //console.log(res);
+        dispatch({type:'GET_COURSES',payload:res.data})
+    })
+  }
 }
