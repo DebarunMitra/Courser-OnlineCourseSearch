bb5282d49fff7f0a14b443a703cd6aa3742a6e0f
DebarunMitra <debarun96mitra@gmail.com>
2019-12-13 17:28:22 +0530

    search box modified

--- client/src/components/search-box/search-box.component.jsx
+++ client/src/components/search-box/search-box.component.jsx
@@ -4,20 +4,44 @@ import './search-box.style.css';
 
 
 class SearchBox extends Component{
+  constructor(props){
+    super(props);
+
+    this.state={
+      searchUpdate:'',
+      finalSearch:''
+    }
+  }
+
+  updateSearch=e=>{
+    this.setState({
+      searchUpdate:e.target.value
+    })
+  }
+
+  getSearch=e=>{
+    e.preventDefault();
+    this.setState({
+      finalSearch:this.state.searchUpdate
+    })
+  }
+
   render(){
      const {search,updateSearch,placeholder}=this.props;
      return (
        <div className="search-box">
-          <input
-            type="text"
-            className="search-bar"
-            value={search}
-            onChange={updateSearch}
-            placeholder={placeholder}
-            />
-        <button type="submit" className="search-button">
-          <i className="fas fa-search" />
-        </button>
+          <form onSubmit={this.getSearch}>
+            <input
+              type="text"
+              className="search-bar"
+              value={search}
+              onChange={this.updateSearch}
+              placeholder={placeholder}
+              />
+              <button type="submit" className="search-button">
+                <i className="fas fa-search" />
+              </button>
+          </form>
       </div>
 
   );
